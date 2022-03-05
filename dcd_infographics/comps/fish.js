8c6af89529288cc1c3186185807ad22899de53cd
ykim312 <ykim312@my.bcit.ca>
2022-03-04 23:48:57 -0800

    adding new fish images, fish.js, corals

---
+++ dcd_infographics/comps/fish.js
@@ -0,0 +1,38 @@
+//MUST HAVE - CREATE A TEMPLATE TAG
+var template_fish = document.createElement("template"); //<template> </template> RULE
+
+//To-do - CREATE THE UI HERE!
+template_fish.innerHTML = `
+
+<div id='fish_image'>
+        <img id='fish' src='/img/Fish_Black.png'/>
+</div>
+
+`;
+
+//MUST HAVE - CREATE A CLASS WITH HTMLELEMENT POWERS (interfaces/functionalities)
+class TheFish extends HTMLElement {
+
+    //MUST HAVE - CREATE A CONSTRUCTOR TO DO INITAL ASSOCIATIONS
+    constructor(){
+        super(); //pass on the HTMLElement super powers!
+        this.attachShadow({mode:"open"}) //Attach it to the shadowRoot
+
+        //To-do - CREATE THE STATES FOR THE UI HERE!
+    }
+
+    //MUST HAVE - FUNCTION THAT RUNS AFTER IT'S CONNECTED
+    connectedCallback(){
+        this.shadowRoot.appendChild(template_fish.content.cloneNode(true)); //use the template to make a clone
+
+    if(this.getAttribute("img")){
+        this.shadowRoot.querySelector("#fish_image > img").src = this.getAttribute("img");
+        }
+    
+    }
+
+    //To-do - CREATE THE FUNCTIONALITIES HERE!
+}
+
+//MUST HAVE - define the tag for the custom elements
+customElements.define("the-fish", TheFish)
